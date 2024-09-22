778b49f0ff53440f7dbd1acd61ff76d1654b26e4
Vishnu Mouli <vishnumouli0@gmail.com>
2024-09-22 19:05:14 +0530

    configured pre commit hook

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,3 @@
-import Image from "next/image";
-
 export default function Home() {
-  return (
-    <div className="">
-     Hello World
-    </div>
-  );
+  return <div className="text-xl">Hello World</div>;
 }
