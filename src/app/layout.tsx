1592a4bea6157c9d735b2d1d0c564530461e74bd
Vishnu Mouli <vishnumouli0@gmail.com>
2024-10-05 16:52:25 +0530

    Updated Imports and Exports in Components folder.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,7 @@ import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
 import localFont from "next/font/local";
 
-import { ThemeProvider } from "@/components/themes/theme-provider";
-import ThemeSwitcher from "@/components/themes/theme-switcher";
+import { ThemeProvider, ThemeSwitcher } from "@/components";
 import { cn } from "@/lib/utils";
 
 import { MoonIcon, SunIcon } from "../../public/svgs";
---
+++ src/components/index.tsx
@@ -0,0 +1,6 @@
+import { ThemeProvider } from "./themes/theme-provider";
+import ThemeSwitcher from "./themes/theme-switcher";
+import { Button } from "./ui/button";
+import { Switch } from "./ui/switch";
+
+export { Button, Switch, ThemeProvider, ThemeSwitcher };
